6eacf49e9bff41f6a3ed897cea7cd048ef7def13
Raghavi Mohan <70186244+Raghavi-Mohan@users.noreply.github.com>
2024-10-24 16:14:19 -0400

    new angular project
    description

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CatalogcomponentComponent } from './catalogcomponent/catalogcomponent.component';
+import { PaintingcomponentComponent } from './paintingcomponent/paintingcomponent.component';
+import { HomecomponentComponent } from './homecomponent/homecomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatalogcomponentComponent,
+    PaintingcomponentComponent,
+    HomecomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
